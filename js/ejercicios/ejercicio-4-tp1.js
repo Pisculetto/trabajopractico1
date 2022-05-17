3126cc1462ca70da70568a9c825add0972cec91f
Hector Norry <piscudelag@gmail.com>
2022-05-17 06:53:09 -0300

    Ejercicios hechos hasta el 5

---
+++ js/ejercicios/ejercicio-4-tp1.js
@@ -0,0 +1,2 @@
+let nombreUsuario = prompt('Ingrese su nombre de usuario: ');
+document.write('Su usuario es:<b> ' + nombreUsuario);
---
+++ js/ejercicios/ejercicio-5-tp1.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt('Ingrese el primer numero: '));
+let numero2 = parseInt(prompt('Ingrese el primer numero: '));
+document.write('La suma del numero 1 y el numero 2 es= <b> '+ (numero1 + numero2));
