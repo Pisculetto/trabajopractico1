ed3a187aa63aeb3497b8a2db42f47500499ad5df
Hector Norry <piscudelag@gmail.com>
2022-05-17 08:19:56 -0300

    se agregan ejercicios 6 7 y 8

---
+++ js/ejercicios/ejercicio-8-tp-1.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt('Ingrese el numero: '));
+
+if(numero % 2 == 0)
+{
+    document.write('El numero ' +numero +' es par');
+}
+
+else
+document.write('El numero ' +numero+ ' no es par');
